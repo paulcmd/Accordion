6e6cc7ded5136bd44bc9bac779b1385a44472bea
paulcmd <choxmail.pm@gmail.com>
2021-03-30 03:36:06 -0700

    added results state

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,18 +3,24 @@ import axios from 'axios'
 
 const Search = () => {
     const [term, setTerm] = useState('')
+    const [results, setResults] = useEffect([])
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
-                params: {
-                    action: 'query',
-                    list: 'search',
-                    origin: '*',
-                    format: 'json',
-                    srsearch: term
+            const { data } = await axios.get(
+                'https://en.wikipedia.org/w/api.php',
+                {
+                    params: {
+                        action: 'query',
+                        list: 'search',
+                        origin: '*',
+                        format: 'json',
+                        srsearch: term
+                    }
                 }
-            })
+            )
+
+            setResults(data)
         }
 
         search()
