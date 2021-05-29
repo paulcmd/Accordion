236ee303f630c78ae4750e00b98f043c2c433aa0
paulcmd <choxmail.pm@gmail.com>
2021-05-29 14:09:39 -0700

    added Link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const Link = ({ href, className, children }) => {
-
     const onClick = (e) => {
+        if (e.metaKey || e.ctrlKey) {
+            return
+        }
+
         e.preventDefault()
         window.history.pushState({}, '', href)
 
@@ -19,10 +22,12 @@ const Link = ({ href, className, children }) => {
 
 export default Link
 
-
 /*
 Link component is an a tag that receives a href, classname and children(text). onClick prevents
 default action so that page is not refreshed.
 
 -window.history.pushState({}, '', href) displays correct href on address bar
+
+5- if ctrl key is pressed return early ie dont do 9-13. go to 16. this is
+because we only need to display that one link. 
 */
